f40f0a470124517930bbc3058dd3f97bceb5ecd3
Tomas Karasek <tom.to.the.k@gmail.com>
2023-03-23 15:04:36 +0100

    change first version to v0.1.0

--- equinix_metal/equinix_metal/__init__.py
+++ equinix_metal/equinix_metal/__init__.py
@@ -17,7 +17,7 @@
 
 from __future__ import absolute_import
 
-__version__ = "0.0.1"
+__version__ = "0.1.0"
 
 # import apis into sdk package
 from equinix_metal.api.devices_api import DevicesApi
--- equinix_metal/equinix_metal/configuration.py
+++ equinix_metal/equinix_metal/configuration.py
@@ -396,7 +396,7 @@ conf = equinix_metal.Configuration(
                "OS: {env}\n"\
                "Python Version: {pyversion}\n"\
                "Version of the API: 1.0.0\n"\
-               "SDK Package Version: 0.0.1".\
+               "SDK Package Version: 0.1.0".\
                format(env=sys.platform, pyversion=sys.version)
 
     def get_host_settings(self):
